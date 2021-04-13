15889e761a2befe906d4617d27dacffdadd91a0f
gentlekboy <gentlekboy@gmail.com>
2021-04-13 22:36:41 +0100

    Added JS to control menu toggling

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,7 @@
 "use strict"
+
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', function (){
+    document.getElementById('navLinkContainer').classList.toggle('showNavLinks');
+})
